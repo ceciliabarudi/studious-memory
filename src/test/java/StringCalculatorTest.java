0cdf01682849fdfbe4c42f9afcbd6f025a8da962
Bruno Belarte <belarte@Brunos-MacBook-Pro.local>
2021-06-15 15:56:39 +0200

    Make StringCalculator a class member

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,19 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+
+    }
 
     @Test
     void should_return_0_with_empty_string() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int value = stringCalculator.add("");
-        assertEquals(0, value);
+        assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     void should_return_correct_value_for_single_parameter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
         assertEquals(5, stringCalculator.add("5"));
         assertEquals(199, stringCalculator.add("199"));
@@ -21,26 +26,22 @@ public class StringCalculatorTest {
 
     @Test
     void should_return_correct_value_for_two_parameters() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("1,2"));
     }
 
     @Test
     void should_return_correct_value_for_unknown_amount_of_parameters() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(10, stringCalculator.add("1,2,3,4"));
         assertEquals(20, stringCalculator.add("1,5,8,1,5"));
     }
 
     @Test
     void can_handle_new_line_as_delimiter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
     @Test
     void can_define_custom_delimiter() {
-        StringCalculator stringCalculator = new StringCalculator();
         assertEquals(3, stringCalculator.add("//;\n1;2"));
     }
 }
